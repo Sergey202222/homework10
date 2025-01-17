67f9ecb3c169a9077b6a56d356e90e3858b994e3
Sergey <57405954+Sergey202222@users.noreply.github.com>
2025-01-18 00:36:42 +0300

    Add files via upload

---
+++ homework10.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int sizeArray{};
+    std::cout << "Введите размер массива: ";
+    std::cin >> sizeArray;
+    int* arr{ new int[sizeArray]() };
+
+    for (int index{}; index < sizeArray; ++index)
+    {
+        std::cout << "arr[" << index << "] = ";
+        std::cin >> arr[index];
+    }
+
+    std::cout << "Введённый массив: ";
+    for (int index{}; index < sizeArray; ++index)
+    {
+        std::cout << arr[index] << ' ';
+    }
+
+    delete[] arr;
+    return EXIT_SUCCESS;
+}
