8b6af2f468fbcdc53eecbabae39fadc6a2e3cf0a
Sergey <57405954+Sergey202222@users.noreply.github.com>
2025-01-18 00:37:25 +0300

    Add files via upload

---
+++ homework10.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+double* create_array(int size)
+{
+    return new double[size]();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Введите размер массива: ";
+    int sizeArray{};
+    std::cin >> sizeArray;
+    double* arr{ create_array(sizeArray) };
+
+    std::cout << "Массив: ";
+    for (int index{}; index < sizeArray; ++index)
+    {
+        std::cout << arr[index] << ' ';
+    }
+
+    delete[] arr;
+    return EXIT_SUCCESS;
+}
---
+++ homework10.3.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+int** create_two_dim_array(int rows, int colums);
+void fill_two_dim_array(int** matrix, int rows, int columns);
+void print_two_dim_array(int** matrix, int rows, int columns);
+void delete_two_dim_array(int** matrix, int rows, int columns);
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int rows{}, columns{};
+    std::cout << "Введите количество строк: ";
+    std::cin >> rows;
+    std::cout << "Введите количество столбцов: ";
+    std::cin >> columns;
+
+    int** matrix{create_two_dim_array(rows, columns)};
+    fill_two_dim_array(matrix, rows, columns);
+    print_two_dim_array(matrix, rows, columns);
+    delete_two_dim_array(matrix, rows, columns);
+
+    return EXIT_SUCCESS;
+}
+
+int** create_two_dim_array(int rows, int columns)
+{
+    int** matrix{ new int* [rows] };
+    for (int row{}; row < rows; ++row)
+    {
+        matrix[row] = new int[columns];
+    }
+
+    return matrix;
+}
+
+void fill_two_dim_array(int** matrix, int rows, int columns)
+{
+    for (int row{}; row < rows; ++row)
+    {
+        for (int column{}; column < columns; ++column)
+        {
+            matrix[row][column] = (row + 1) * (column + 1);
+        }
+    }
+}
+
+void print_two_dim_array(int** matrix, int rows, int columns)
+{
+    std::cout << "Таблица умножения :\n";
+    for (int row{}; row < rows; ++row)
+    {
+        for (int column{}; column < columns; ++column)
+        {
+            std::cout << matrix[row][column] << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
+void delete_two_dim_array(int** matrix, int rows, int columns)
+{
+    for (int row{}; row < rows; ++row)
+    {
+        delete[] matrix[row];
+    }
+    delete[] matrix;
+}
